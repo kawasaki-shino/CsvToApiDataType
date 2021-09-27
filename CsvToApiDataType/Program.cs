4731efdc3993773356dabe6e0f6af0d8cfec711b
sakuraba-yusuke <sakuraba_yusuke@nanairo-inc.jp>
2021-09-27 16:28:39 +0900

    文字コードを判定してから読み取るように修正

--- CsvToApiDataType/Program.cs
+++ CsvToApiDataType/Program.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Hnx8.ReadJEnc;
+using System;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
-using CsvHelper;
-using CsvHelper.Configuration;
 
 namespace CsvToApiDataType
 {
@@ -15,7 +16,8 @@ namespace CsvToApiDataType
 		static void Main(string[] args)
 		{
 			var files = args.ToList();
-			if (args.Length == 0 && File.Exists(TARGET))			{
+			if (args.Length == 0 && File.Exists(TARGET))
+			{
 				files.Add(TARGET);
 			}
 
@@ -31,13 +33,44 @@ namespace CsvToApiDataType
 				HasHeaderRecord = false
 			};
 
+
 			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 			foreach (var file in files)
 			{
-				using var reader = new StreamReader(file, Encoding.GetEncoding("SHIFT_JIS"));
+				// 文字コード判定
+				byte[] bytes;
+				using (var filecheck = new FileStream(file, FileMode.Open))
+				{
+					bytes = new byte[filecheck.Length];
+					filecheck.Read(bytes, 0, bytes.Length);
+				}
+
+				var encode = ReadJEnc.JP.GetEncoding(bytes, bytes.Length, out _);
+
+				var enc = string.Empty;
+				switch (encode.Name)
+				{
+					case "UTF-8N":
+					case "UTF-8":
+						enc = "UTF-8";
+						break;
+					case "ShiftJIS":
+						enc = "SHIFT-JIS";
+						break;
+				}
+
+				if (string.IsNullOrWhiteSpace(enc))
+				{
+					Console.WriteLine("[エラー] 文字コードは、UTF-8N, UTF-8, SHIFT-JIS のいずれかにしてください。");
+					return;
+				}
+
+				// 読み取り
+				using var reader = new StreamReader(file, Encoding.GetEncoding(enc));
 				using var csv = new CsvReader(reader, config);
 				var list = csv.GetRecords<Data>().ToList();
 
+				// 書き込み
 				var desktop = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
 				var path = Path.Combine(desktop, $"{Path.GetFileNameWithoutExtension(file)}.yaml");
 				using var fs = File.Create(path);
@@ -47,6 +80,8 @@ namespace CsvToApiDataType
 				sw.WriteLine($"{new string(' ', 4)}{Path.GetFileNameWithoutExtension(file)}:");
 				sw.WriteLine($"{new string(' ', 6)}description:");
 				sw.WriteLine($"{new string(' ', 6)}required:");
+
+				// 必須部分書き込み
 				foreach (var entity in list)
 				{
 					if (entity.Converted必須) sw.WriteLine($"{new string(' ', 6)}- {entity.物理名PascalCase}");
@@ -54,6 +89,7 @@ namespace CsvToApiDataType
 				sw.WriteLine($"{new string(' ', 6)}type: object");
 				sw.WriteLine($"{new string(' ', 6)}properties:");
 
+				// プロパティ部分書き込み
 				foreach (var entity in list)
 				{
 					sw.WriteLine($"{new string(' ', 8)}{entity.物理名PascalCase}:");
